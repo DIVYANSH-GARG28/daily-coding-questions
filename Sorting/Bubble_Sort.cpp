3ed22877304793ce4aa34e69ddb5b5ffe402c3bd
Divyansh_Garg <divyansh2282006@gmail.com>
2026-07-15 22:17:38 +0530

    Updates

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int> input(n);
+    int arr[n];
     for(int i = 0; i<n;i++){
-        cin>>input[i];
+        cin>>arr[i];
     }
     for(int i = 0; i<n;i++){
         for(int j = 0; j<n-1;j++){
-            if(input[j]>input[j+1]){
-                swap(input[j],input[j+1]);
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
             }
         }
     }
-    for(auto v: input){
-        cout<<v<<" ";
+    for(int i = 0; i<n;i++){
+        cout<<arr[i]<<" ";
     }
 }
